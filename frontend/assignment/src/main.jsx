2b78c9c7f1059269504988481a85052f48c4bac5
deepakkurella24 <deepak.kurella24@sasi.ac.in>
2026-03-15 21:47:14 +0530

    deployment fix

--- frontend/assignment/src/main.jsx
+++ frontend/assignment/src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/index.css'
 import App from './App.jsx'
-import { store } from './store.js'
+import { store } from './app/store.js'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
